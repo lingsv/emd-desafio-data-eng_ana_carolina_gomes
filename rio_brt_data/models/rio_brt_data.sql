023378c6de2481b134c0e8317054bc58d025a0d9
Ana Carolina Gomes <45212671+lingsv@users.noreply.github.com>
2023-08-01 11:51:02 -0300

    remove unused comments

--- rio_brt_data/models/rio_brt_data.sql
+++ rio_brt_data/models/rio_brt_data.sql
@@ -1,18 +1,6 @@
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-
 {{ config(materialized='table', schema='public', alias='rio_brt_data') }}
 
--- Defina a estrutura da tabela resultante
--- Certifique-se de ajustar os tipos de dados conforme necessário
--- O DBT criará a tabela automaticamente com base na consulta SELECT
 SELECT
     codigo,
     placa,
@@ -29,8 +17,3 @@ SELECT
 
 FROM {{ source('rio_brt_data', 'rio_brt_data') }}
 
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
